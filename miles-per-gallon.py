32fa299a7513fc915f957e030bf26c1e32d65db7
zacharytyhacz <zactyh@gmail.com>
2023-05-08 22:50:46 -0400

    Added MPG example

---
+++ miles-per-gallon.py
@@ -0,0 +1,26 @@
+print("              --      --     ")
+print("            .:\"  | .:'\" |     ")
+print("          --  ___   ___  -     ")
+print("        /:.  /  .\ /.  \ .\     ")
+print("       |:|. ;\___/O\___/  :|     ")
+print("       |:|. |  `__|__'  | .|     ")
+print("       |:|.  \_,     ,_/  /     ")
+print("        \______       |__/     L A S A G N A ")
+print("         |:.           \     ")
+print("        /.:,|  |        \     ")
+print("       /.:,.|  |         \     ")
+print("       |::.. \_;_\-;       |     ")
+print(" _____|::..    .::|       |     ")
+print("/   ----,     .::/__,    /__,     ")
+print("\_______|,...____;_;_|../_;_|     ")
+print("")
+print(" Welcome to my program.... ")
+
+
+miles = float(input("How many miles you drove: "))
+gallons = float(input("How many gallons consumed: "))
+gallon_price = float(input("Price of one gallon of gas: "))
+
+print("Your MPG is: ", miles / gallons)
+print("Cost of trip was $", gallons * gallon_price)
+
